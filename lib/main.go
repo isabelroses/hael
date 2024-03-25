d604e456f3f1b1c5df011204adb74d7e27a1fce5
isabel <isabel@isabelroses.com>
2024-03-25 21:22:35 +0000

    feat(lib): re-add genderTernary as neutral is default often

--- lib/main.go
+++ lib/main.go
@@ -14,3 +14,16 @@ func GenderReduce(gender string, f string, m string, n string, d string) string 
 	}
 	return out
 }
+
+func GenderTernary(gender string, f string, m string, d string) string {
+	var out string
+	switch gender {
+	case "fem":
+		out = f
+	case "masc":
+		out = m
+	default:
+		out = d
+	}
+	return out
+}
